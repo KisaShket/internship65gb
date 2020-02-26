ce31d151c999521d95e555562752eecc43351a81
Miska <nik55-55@mail.ru>
2020-02-26 14:01:27 +0400

    Initial Commit

---
+++ internship65gb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  internship65gb
+//
+//  Created by Miska  on 26.02.2020.
+//  Copyright © 2020 Miska . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
